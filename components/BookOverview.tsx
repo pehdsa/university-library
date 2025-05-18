e023bc3d22ed18878610c739b5303af54704a7f1
Pedro Henrique <pehdsa@gmail.com>
2025-05-17 23:12:59 -0300

    fix: correçao para mostrar o botao de borrow

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -30,8 +30,6 @@ export async function BookOverview({
     .where(eq(users.id, userId))
     .limit(1);
 
-  if (!user) return null;
-
   const borrowingEligibility = {
     isEligible: availableCopies > 0 && user.status === "APPROVED",
     message:
@@ -70,11 +68,13 @@ export async function BookOverview({
 
         <p className="book-description">{description}</p>
 
-        <BorrowBook
-          bookId={id}
-          userId={userId}
-          borrowingEligibility={borrowingEligibility}
-        />
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
 
       <div className="relative flex flex-1 justify-center">
